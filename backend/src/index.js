6ccde715e2acf12149d67cfdfb90a2a89f7cb43e
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-09-15 22:10:28 +0530

    added database connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,13 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import { PORT } from "./config/serverConfig.js";
+import { connectDB } from "./config/dbConfig.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Express server is Running");
@@ -9,4 +15,5 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`server is running on PORT: ${PORT}`);
+  connectDB();
 });
