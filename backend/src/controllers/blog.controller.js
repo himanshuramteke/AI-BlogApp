77b63da3d50291bd0fce328abdaf92082e2814bb
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-09-17 10:55:58 +0530

    Added get, delete and toogle-published controllers

--- backend/src/controllers/blog.controller.js
+++ backend/src/controllers/blog.controller.js
@@ -60,3 +60,78 @@ export const addBlogController = async (req, res) => {
     });
   }
 };
+
+export const getAllBlogsController = async (req, res) => {
+  try {
+    const blogs = await Blog.find({ isPublished: true });
+    res.status(200).json({
+      success: true,
+      blogs,
+    });
+  } catch (error) {
+    console.log("Error in getAllBlogs controller", error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const getBlogByIdController = async (req, res) => {
+  try {
+    const { blogId } = req.params;
+    const blog = await Blog.findById(blogId);
+
+    if (!blog) {
+      return res.status(404).json({
+        success: false,
+        message: "Blog not found",
+      });
+    }
+
+    res.status(200).json({ success: true, blog });
+  } catch (error) {
+    console.log("Error in getBlogById controller", error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const deleteBlogByIdController = async (req, res) => {
+  try {
+    const { id } = req.body;
+    await Blog.findByIdAndDelete(id);
+    res.status(200).json({
+      success: true,
+      message: "Blog deleted successfully",
+    });
+  } catch (error) {
+    console.log("Error in deleteBlogById controller", error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const togglePublishBlogController = async (req, res) => {
+  try {
+    const { id } = req.body;
+    const blog = await Blog.findById(id);
+    blog.isPublished = !blog.isPublished;
+    await blog.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Blog status updated",
+    });
+  } catch (error) {
+    console.log("Error in togglePublishBlogController", error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- backend/src/routes/blog.route.js
+++ backend/src/routes/blog.route.js
@@ -1,10 +1,24 @@
 import express from "express";
-import { addBlogController } from "../controllers/blog.controller.js";
+import {
+  addBlogController,
+  deleteBlogByIdController,
+  getAllBlogsController,
+  getBlogByIdController,
+  togglePublishBlogController,
+} from "../controllers/blog.controller.js";
 import upload from "../middlewares/multer.middleware.js";
 import { protectRoute } from "../middlewares/protect.middleware.js";
 
 const router = express.Router();
 
 router.post("/add", upload.single("image"), protectRoute, addBlogController);
+
+router.get("/all", getAllBlogsController);
+
+router.get("/:blogId", getBlogByIdController);
+
+router.delete("/deleteId", protectRoute, deleteBlogByIdController);
+
+router.post("/toggle-publish", protectRoute, togglePublishBlogController);
 
 export default router;
