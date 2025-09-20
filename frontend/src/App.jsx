f4639ad19efc17ed28f478d4b79e2d87d054cbe1
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-09-20 22:32:15 +0530

    added Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,14 @@
-function App() {
-  return <p className="text-3xl bg-amber-400"> Tailwind CSS</p>;
-}
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import BlogPage from "./pages/BlogPage";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/blog" element={<BlogPage />} />
+    </Routes>
+  );
+};
 
 export default App;
