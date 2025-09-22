3fd6058baebfaf238adc20d1e0326e8546796925
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-09-22 22:11:23 +0530

    remove toggle mode button

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,26 +1,7 @@
-import { useEffect, useState } from "react";
-import { Moon, Sun } from "lucide-react";
 import { useNavigate } from "react-router";
 
 const Navbar = () => {
   const navigate = useNavigate();
-
-  const [theme, setTheme] = useState(
-    localStorage.getItem("theme") || "dark" // default to dark
-  );
-
-  useEffect(() => {
-    if (theme === "dark") {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-    localStorage.setItem("theme", theme);
-  }, [theme]);
-
-  const toggleTheme = () => {
-    setTheme(theme === "light" ? "dark" : "light");
-  };
 
   return (
     <nav className="relative bg-gray-900/95 backdrop-blur-md z-50 transition-colors duration-300">
@@ -46,18 +27,6 @@ const Navbar = () => {
             className="px-5 py-2.5 rounded-lg text-sm font-medium text-gray-200 bg-gray-800 hover:bg-gray-700 transition-colors duration-200 cursor-pointer"
           >
             Signup
-          </button>
-
-          {/* Theme Toggle */}
-          <button
-            onClick={toggleTheme}
-            className="p-2 rounded-lg bg-gray-800 hover:bg-gray-700 text-gray-200 transition-colors duration-200"
-          >
-            {theme === "light" ? (
-              <Moon className="w-5 h-5" />
-            ) : (
-              <Sun className="w-5 h-5" />
-            )}
           </button>
         </div>
       </div>
